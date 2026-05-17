d68af355d3ba3f32dbc6401deb6d3eb433926aff
Muktar <muktaradamu677@gmail.com>
2026-05-17 03:01:37 +0100

    Fix Home.jsx Hero import casing for Vercel build

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "../Component/hero";
+import Hero from "../Component/Hero";
 import WorkDetailCard from "../Component/WorkDetailCard";
 import GoalAndOutcome from "../Component/goalAndOutcome";
 
